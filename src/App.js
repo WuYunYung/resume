6168cf8e4c6afcc6e421d76060b317f0902f3020
WilsonWu <wilson.wu.yy@gmail.com>
2022-04-07 03:35:27 +0800

    ✨ feat(base): 工作内容更新

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
           <Title>个人信息</Title>
 
           <div className="flex gap-4">
-            <div className="w-24 h-24 rounded-full overflow-hidden ">
+            <div className="w-24 h-24 rounded-full overflow-hidden shadow">
               <img
                 src="https://avatars.githubusercontent.com/u/43036689?v=4"
                 alt=""
@@ -65,6 +65,72 @@ function App() {
               <div className="flex justify-between">
                 <div className="flex flex-col gap-2">
                   <div className="flex gap-4">
+                    <h2 className="text-md font-bold">
+                      广东马上信息科技有限公司
+                    </h2>
+                    <div>
+                      <Tag>2021-05</Tag> - <Tag>至今</Tag>
+                    </div>
+                  </div>
+                  <p className="text-sm flex gap-2">
+                    <span className="font-bold">主要工作内容：</span>
+                    <span className="text-gray-500">
+                      云课堂相关业务开发与维护
+                    </span>
+                  </p>
+                </div>
+                <div className="text-sm">
+                  <span className="font-bold">职位：</span>
+                  <span className="text-gray-500">前端开发工程师</span>
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <div className="grid grid-cols-2 gap-2">
+                  <ProjectCard
+                    title="鹤壁智慧教育平台"
+                    address="https://edu.hebi.cn/"
+                    tags={["lerna", "nuxt"]}
+                  >
+                    基于Nuxt的SSR项目
+                  </ProjectCard>
+                  <ProjectCard
+                    title="智慧教育云"
+                    address="https://m.vxiao.cn/"
+                    tags={["vue", "vue-router", "vuex", "sass", "i18n"]}
+                  >
+                    主要负责其中直播业务的内容
+                  </ProjectCard>
+                  <ProjectCard
+                    title="云课堂*"
+                    tags={[
+                      "vue",
+                      "vue-router",
+                      "vuex",
+                      "sass",
+                      "mpegts.js",
+                      "TRTC",
+                    ]}
+                  >
+                    <p>主要功能为直播、互动</p>
+                    <p>主要负责上下线授权等逻辑部分的重构</p>
+                    <p>流功能主要通过 mpegts.js & TRTC 实现</p>
+                  </ProjectCard>
+                  <ProjectCard
+                    title="移动端基础建设*"
+                    tags={["react", "hooks", "taro", "typescript"]}
+                  >
+                    <p>同构移动端和微信小程序</p>
+                    <p>主要负责弹窗类基础组件开发，以及通用功能基座开发</p>
+                  </ProjectCard>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="flex flex-col gap-2">
+              <div className="flex justify-between">
+                <div className="flex flex-col gap-2">
+                  <div className="flex gap-4">
                     <h2 className="text-md font-bold">Kanar Profiles</h2>
                     <div>
                       <Tag>2019-07 </Tag> - <Tag>2021-05</Tag>
@@ -109,62 +175,17 @@ function App() {
               <div className="flex justify-between">
                 <div className="flex flex-col gap-2">
                   <div className="flex gap-4">
-                    <h2 className="text-md font-bold">
-                      广东马上信息科技有限公司
-                    </h2>
-                    <div>
-                      <Tag>2021-05</Tag> - <Tag>至今</Tag>
-                    </div>
+                    <h2 className="text-md font-bold">个人项目</h2>
                   </div>
-                  <p className="text-sm flex gap-2">
-                    <span className="font-bold">主要工作内容：</span>
-                    <span className="text-gray-500">
-                      云课堂相关业务开发与维护
-                    </span>
-                  </p>
-                </div>
-                <div className="text-sm">
-                  <span className="font-bold">职位：</span>
-                  <span className="text-gray-500">前端开发工程师</span>
                 </div>
               </div>
               <div className="flex flex-col gap-2">
-                <div className="grid grid-cols-2 gap-2">
-                  <ProjectCard
-                    title="鹤壁智慧教育平台"
-                    address="https://edu.hebi.cn/"
-                    tags={["lerna", "nuxt"]}
-                  >
-                    基于Nuxt的SSR项目，主要难点是日历组件的开发
-                  </ProjectCard>
-                  <ProjectCard
-                    title="智慧教育云"
-                    address="https://m.vxiao.cn/"
-                    tags={["vue", "vue-router", "vuex", "sass", "i18n"]}
-                  >
-                    主要负责其中直播业务的内容，包括设置，报表，卡片等等
-                  </ProjectCard>
-                </div>
                 <ProjectCard
-                  title="云课堂"
-                  tags={[
-                    "vue",
-                    "vue-router",
-                    "vuex",
-                    "sass",
-                    "mpegts.js",
-                    "TRTC",
-                  ]}
+                  title="zs-site"
+                  tags={["vue", "Nuxtjs", "tailwindcss"]}
+                  address="http://zs-site-5g0jtncv63f52ccd-1259648041.tcloudbaseapp.com/"
                 >
-                  <p className="text-sm text-gray-500">
-                    云课堂主项目，主要负责重构、新功能和维护
-                  </p>
-                  <p className="text-sm text-gray-500">
-                    项目难点主要在于，不同层级的逻辑封装，以及涉及socket的主/被动逻辑
-                  </p>
-                  <p className="text-sm text-gray-500">
-                    流功能主要通过 mpegts.js & TRTC 实现
-                  </p>
+                  可以运行在多种设备上的官网
                 </ProjectCard>
               </div>
             </div>
